06f081ccfe0a9a044ca5f73bb08fe7b21caaa9a3
steve-ongera <gadafisteve001@gmail.com>
2025-01-26 23:22:56 +0300

    styled my login.html and configured static files in settings.py

--- two_way_auth/settings.py
+++ two_way_auth/settings.py
@@ -107,6 +107,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# settings.py
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Point to the 'static' folder at the root of your project
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
